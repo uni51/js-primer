5e958be058fbdd692c3df2c600242b6f9cee6db7
Kouichi Shimomura <kouichi.shimomura@pasonadigisol.jp>
2021-05-14 18:17:57 +0900

    22.7.1 Async Functionの定義

---
+++ async/22-7-1.js
@@ -0,0 +1,14 @@
+// 22.7 Async Function
+// 22.7.1 Async Functionの定義
+
+// 関数宣言のAsync Function版
+async function fn1() {}
+
+// 関数式のAsync Function版
+const fn2 = async function() {};
+
+// Arrow FunctionのAsync Function版
+const fn3 = async() => {};
+
+// メソッドの短縮記法のAsync Function版
+const obj = { async method() {} };
